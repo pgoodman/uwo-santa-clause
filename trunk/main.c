063227b09e420eae5e455ab50c5d19d17749cff6
peter.goodman <peter.goodman@a7182b40-dfb5-11de-b4b3-6d349e4f6f86>
2009-12-03 05:50:54 +0000

    cleaned up some of the code, still need to add comments explaining usage of global variables.

--- trunk/main.c
+++ trunk/main.c
@@ -23,6 +23,8 @@
 #define NUM_ELVES_PER_GROUP 3
 #define MAX_MESSAGE_LENGTH 100
 
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
 /* the sets of semaphores used for the solution.
  *
  * The first is for a few miscellaneous semaphores needed to see if santa is
@@ -39,10 +41,7 @@ static sem_set_t elf_line_set;
 static sem_t santa_busy_mutex;
 static sem_t santa_sleep_mutex;
 
-static sem_t id_counter_mutex;
 static sem_t reindeer_counting_sem;
-
-
 
 static set_t elves_waiting;
 static sem_t elf_counting_sem;
@@ -55,52 +54,17 @@ static sem_t elf_counter_lock;
 static int num_elves_being_helped = 0;
 
 /**
- * ----------------------------------------------------------------------------
- * General
- * ----------------------------------------------------------------------------
- */
-
-/**
  * Busy wait for an arbitrary amount of time. Before waiting, print out a
  * message to standard output. The message must contain one integer formatting
- * variable. If the message does not contain an integer formatting variable
- * then the integer passed should be -1.
+ * variable.
  *
  * Params: - Message to print
  *         - Integer to substitute into the message
  */
 static void random_wait(const char *message, const int format_var) {
     unsigned int i = rand() % MAX_WAIT_TIME;
-    char formatted_message[MAX_MESSAGE_LENGTH];
-
-    strncpy(&(formatted_message[0]), message, MAX_MESSAGE_LENGTH);
-    if(-1 != format_var) {
-        snprintf(formatted_message, MAX_MESSAGE_LENGTH, message, format_var);
-    }
-
-    fprintf(stdout, "%s", formatted_message);
+    fprintf(stdout, message, format_var);
     for(; --i; ) /* ho ho ho! */;
-}
-
-/**
- * Get the current value of a counter and then increment it. Note: this enforces
- * mutual exclusion, thus if there are two distinct counters then incrementing
- * one counter will be mutually exclusive with another. While this isn't
- * desirable, it has a negligible affect on this solution, and is really not
- * the focus of the problem at hand.
- *
- * Params: Pointer to the counter
- */
-static int get_value_and_increment(int *counter) {
-    int id;
-
-    /* lock the counter so we can get a unique id */
-    CRITICAL(id_counter_mutex, {
-        id = *counter;
-        ++(*counter);
-    });
-
-    return id;
 }
 
 /**
@@ -110,10 +74,9 @@ static int get_value_and_increment(int *counter) {
  */
 
 /**
- * Have santa help the elves.
+ * Have santa help the elves; function required in problem specifications.
  */
 static void help_elves(void) {
-
     int i;
     int elf;
 
@@ -141,35 +104,27 @@ static void help_elves(void) {
 }
 
 /**
- * Prepare the sleigh for the reindeer.
+ * Prepare the sleigh for the reindeer; function required by problem
+ * specification.
  */
 static void prepare_sleigh(void) {
-
-    /* block out elves from getting santa */
+    /* block out elves from getting santa and then free up reindeer  */
     sem_wait(santa_busy_mutex);
-
     fprintf(stdout, "Santa: preparing the sleigh. \n");
-
-    /* tell the reindeer that they are ready to be hitched. */
     sem_signal_ntimes(reindeer_counting_sem, NUM_REINDEER);
 }
 
 /**
- * Run santa's thread.
+ * Santa thread. Note: do not launch more than one!
  */
 static void *santa(void *_) {
-    int i = INT_MAX;
-
-    /* make sure we eventually terminate. */
-    for(; i--; ) {
+    while(1) {
 
         /* wait until santa isn't busy to continue */
-        /*CRITICAL(santa_busy_mutex, {
-            random_wait("Santa: zzZZzZzzzZZzzz (sleeping) \n", -1);
-        });*/
         CRITICAL(santa_busy_mutex, {
             fprintf(stdout, "Santa: zzZZzZzzzZZzzz (sleeping) \n");
         });
+
         sem_wait(santa_sleep_mutex);
         fprintf(stdout, "Santa: I'm up, I'm up! Whaddya want? \n");
 
@@ -189,7 +144,6 @@ static void *santa(void *_) {
             help_elves();
         }
     }
-
     return NULL;
 }
 
@@ -200,10 +154,11 @@ static void *santa(void *_) {
  */
 
 /**
- * Get help from santa.
+ * Get help from santa; function required in problem specifications.
  */
 static void get_help(const int id) {
     fprintf(stdout, "Elf %d got santa's help! \n", id);
+
     CRITICAL(elf_counter_lock, {
         --num_elves_being_helped;
 
@@ -215,10 +170,11 @@ static void get_help(const int id) {
     });
 }
 
-static void *elf(void *_) {
-
-    static int atomic_counter = 0;
-    const int id = get_value_and_increment(&atomic_counter);
+/**
+ * A single elf thread.
+ */
+static void *elf(void *elf_id) {
+    const int id = *((int *) elf_id);
 
     while(1) {
         random_wait("Elf %d is working... \n", id);
@@ -252,45 +208,46 @@ static void *elf(void *_) {
  * ----------------------------------------------------------------------------
  */
 
+/**
+ * Have a reindeer get hitched; function required by problem specifications.
+ */
 static void get_hitched(const int id) {
     fprintf(stdout, "Reindeer %d is getting hitched to the sleigh! \n", id);
 }
 
-static void *reindeer(void *_) {
-
-    static int atomic_counter = 0;
-    const int id = get_value_and_increment(&atomic_counter);
+/**
+ * A single reindeer thread.
+ */
+static void *reindeer(void *reindeer_id) {
+    const int id = *((int *) reindeer_id);
 
     /* have the reindeer go on vacation for an arbitrary amount of time and
      * then come back and wait for the other reindeer to return. */
-    random_wait("Reindeer %d is on vacation... \n", id);
+    random_wait("Reindeer %d is off to the Tropics! \n", id);
 
     CRITICAL(reindeer_counter_lock, {
         ++num_reindeer_waiting;
     });
 
-    fprintf(stdout,
-        "Reindeer %d is back from vacation; waiting for the others.\n",
-        id
-    );
+    fprintf(stdout, "Reindeer %d is back from the Tropics.\n", id);
 
     if(NUM_REINDEER <= num_reindeer_waiting) {
         fprintf(stdout, "Reindeer %d: I'm the last one; I'll get santa!\n", id);
         sem_signal(santa_sleep_mutex);
     }
 
-    /* santa is awake, now wait for him to tell us that he prepared the
-     * sleigh */
+    /* santa is awake, now wait for him to tell us to get hitched */
     sem_wait(reindeer_counting_sem);
 
     /* the sleigh has been prepared, time to get hitched and go! */
     CRITICAL(reindeer_counter_lock, {
+
         get_hitched(id);
         --(num_reindeer_waiting);
 
         /* all reindeer have been hitched, christmas time! */
         if(0 == num_reindeer_waiting) {
-            fprintf(stdout, "Ho ho ho! Off to deliver presents! \n");
+            fprintf(stdout, "Santa: Ho ho ho! Off to deliver presents! \n");
             exit(EXIT_SUCCESS);
         }
     });
@@ -305,54 +262,65 @@ static void *reindeer(void *_) {
  */
 
 /**
- * Launch the threads.
+ * Launch threads in sequence over some arrays of thread ids and arguments.
  */
-static void launch_threads(void) {
-
-    pthread_t thread_ids[1 + NUM_ELVES + NUM_REINDEER];
-    pthread_t *curr_id = &(thread_ids[0]);
-    int j; /* counter for limiting the number of threads launched */
-    void *_ = NULL; /* a value we don't care about */
-
-    /* start up santa, the elves, and the reindeer threads */
-    pthread_create(curr_id++, _, &santa, _);
-
-    for(j = NUM_ELVES; j--; ) {
-        pthread_create(curr_id++, _, &elf, _);
-    }
-
-    for(j = NUM_REINDEER; j--; ) {
-        pthread_create(curr_id++, _, &reindeer, _);
-    }
-
-    /* join all the threads, i.e. wait for all of them to finish */
-    for(--curr_id; curr_id >= &(thread_ids[0]); --curr_id) {
-        pthread_join(*curr_id, &_);
+static void sequence_pthreads(int num_threads,
+                              pthread_t *thread_ids,
+                              void *(*func)(void *),
+                              int *args) {
+    int i;
+    for(i = 0; num_threads--; ++i) {
+        pthread_create(&(thread_ids[i]), NULL, func, (void *) &(args[i]));
     }
 }
 
 /**
- * Free all resources.
+ * Free all resources. Note: performing a set_free as opposed to a
+ * set_exit_free would result (usually) in an error calling free().
  */
 static void free_resources(void) {
     static int resources_freed = 0;
-
     if(!resources_freed) {
         resources_freed = 1;
-        printf("\nFreeing semaphores.\n");
-
+        fprintf(stdout,"\n... And that year was a Merry Christmas indeed!\n\n");
         sem_empty_set(&sem_set);
         sem_empty_set(&elf_line_set);
-
         set_exit_free(elves_waiting);
     }
 }
 
 /**
- * Handle a SIGINT signal.
+ * Handle a SIGINT signal; make it call the at-exit handler.
  */
 static void sigint_handler(int _) {
     exit(EXIT_SUCCESS);
+}
+
+/**
+ * Launch the threads.
+ */
+static void launch_threads(void) {
+
+    pthread_t thread_ids[1 + NUM_ELVES + NUM_REINDEER];
+
+    int ids[MAX(NUM_ELVES, NUM_REINDEER)];
+    int i; /* index into the ids */
+
+    /* fill up the ids */
+    for(i = 0; i < MAX(NUM_ELVES, NUM_REINDEER); ++i) {
+        ids[i] = i;
+    }
+
+    /* start up santa, the elves, and the reindeer threads */
+    pthread_create(&(thread_ids[0]), NULL, &santa, NULL);
+    sequence_pthreads(NUM_ELVES, &(thread_ids[1]), &elf, &(ids[0]));
+    sequence_pthreads(NUM_REINDEER, thread_ids + 1 + NUM_ELVES, &reindeer, ids);
+
+    /* necessary to wait instead of pthread_exit, otherwise stack, and so
+     * values pointed at by ids and thread_ids get corrupted. */
+    for(i = 0; i < (1 + NUM_ELVES + NUM_REINDEER); ++i) {
+        pthread_join(thread_ids[i], NULL);
+    }
 }
 
 /**
@@ -360,7 +328,7 @@ static void sigint_handler(int _) {
  */
 int main(void) {
 
-    sem_fill_set(&sem_set, 8);
+    sem_fill_set(&sem_set, 7);
     sem_fill_set(&elf_line_set, NUM_ELVES);
 
     elves_waiting = set_alloc(NUM_ELVES);
@@ -372,7 +340,6 @@ int main(void) {
          * initialize them. */
 
         sem_unpack_set(&sem_set,
-            &id_counter_mutex,
             &reindeer_counter_lock,
             &elf_counter_lock,
             &santa_busy_mutex,
@@ -383,7 +350,6 @@ int main(void) {
         );
 
         sem_init(elf_mutex, 1);
-        sem_init(id_counter_mutex, 1);
         sem_init(reindeer_counter_lock, 1);
         sem_init(elf_counter_lock, 1);
         sem_init(santa_busy_mutex, 1);
@@ -391,17 +357,16 @@ int main(void) {
         sem_init(reindeer_counting_sem, 0);
         sem_init(elf_counting_sem, NUM_ELVES_PER_GROUP);
 
-        /* initialize all elf semaphores as mutexes that start off locked! */
+        /* initialize all elf semaphores as mutexes that start off *locked* */
         sem_init_all(&elf_line_set, 0);
 
-        /* pseudo-random numbers are used for determining how long elves and
-         * reindeer are working and on vacation respectively. */
+        /* pseudo-random numbers are used for making random-length busy waits.*/
         srand((unsigned int) time(NULL));
 
         launch_threads();
 
     } else {
-        fprintf(stderr, "Unable to register atexit handler.\n");
+        fprintf(stderr, "Unable to register an at-exit handler.\n");
         free_resources();
     }
 
